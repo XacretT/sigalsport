66ab5e7fe6611ce1873314c6a5f544fb5a38f0d4
Michael Kiselov <xacrett@gmail.com>
2020-07-06 20:29:03 +0300

    frontpage upgrade

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -4,15 +4,14 @@
 
 <style>
 	[aria-current] {
-		color: black;
-	}
+		}
 
 	[aria-current]::after {
 		position: absolute;
 		content: '';
 		width: calc(35% - 1em);
 		height: 2px;
-		background-color: gold;
+		background-color: #dfba76;
 		display: block;
 		bottom: 20px;
 	}
@@ -46,26 +45,71 @@
 				<div>
 					<div class="uk-panel">
 						<ul class="uk-nav uk-nav-default">
-							
 							<li class="uk-active"><a href="." class=" menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-19 current_page_item">Home</a></li>
 							<li class="uk-parent"><a href="services" class=" menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">Services</a>
 							<ul class="uk-nav-sub">
-
 								<li><a href="services/tuning" class=" menu-item menu-item-type-post_type menu-item-object-page">Tuning</a></li></ul></li>
 							<li><a href="projects" class=" menu-item menu-item-type-post_type menu-item-object-page">Projects</a></li>
 							<li><a href="team" class=" menu-item menu-item-type-post_type menu-item-object-page">Team</a></li>
 							<li><a href="blog" class=" menu-item menu-item-type-taxonomy menu-item-object-category">Blog</a></li>
 							<li class="uk-parent"><a href="shop" class=" menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">Shop</a>
 							<ul class="uk-nav-sub">
-
 								<li><a href="shop/cart" class=" menu-item menu-item-type-post_type menu-item-object-page">Cart</a></li>
 								<li><a href="shop/account" class=" menu-item menu-item-type-post_type menu-item-object-page">My account</a></li></ul></li>
 						</ul>
 					</div>
 				</div>
-			</div>
-				
+			</div>	
 		</div>
 	</div>
+</div>
 
+
+
+
+
+
+
+<div class="uk-position-relative uk-visible@m">
+    <div class="uk-position-top">
+        <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+            <div class="uk-navbar-center">
+					<div class="uk-navbar-center-left"><div>
+								<ul class="uk-navbar-nav">
+									<li aria-current="{segment === undefined ? 'page' : undefined}"><a href=".">Home</a></li>
+									<li>
+										<a href="#">Services</a>
+										<div class="uk-navbar-dropdown">
+											<ul class="uk-nav uk-navbar-dropdown-nav">
+												<li class="uk-active">Our Services</li>
+												<li><a href="#">Tuning</a></li>
+												<li><a href="#">Painting</a></li>
+											</ul>
+										</div>
+									</li>
+									<li><a href="#">Projects</a></li>
+								</ul>
+							</div></div>
+							<a href="." class="uk-navbar-item uk-logo">
+    <img alt="SigalSport" src="logo.svg" style="height: 70px;"><img class="uk-logo-inverse" alt="SigalSport" src="logo-inverted.svg" style="height: 70px;"></a>
+							<div class="uk-navbar-center-right"><div>
+								<ul class="uk-navbar-nav">
+									<li><a href="#">Team</a></li>
+									<li><a href="#">Blog</a></li>
+									<li>
+										<a href="#">Shop</a>
+										<div class="uk-navbar-dropdown">
+											<ul class="uk-nav uk-navbar-dropdown-nav">
+												<li><a href="#">Cart</a></li>
+												<li><a href="#">My Account</a></li>
+											</ul>
+										</div>
+									</li>
+								</ul>
+					</div></div>
+
+
+            </div>
+        </nav>
+    </div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,13 +10,34 @@
 			<div class="uk-width-1-1">
 				<div class="uk-container">
 					<div class="tm-header-placeholder uk-margin-remove-adjacent" style="height: 80px;"></div>
-					<div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
-						<div class="uk-flex-auto uk-width-1-1@m uk-first-column">
-							<h1 class="uk-h5 uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-top" uk-scrollspy-class="" style="">        Tuning &amp; Performance    </h1>
-							<h2 class="uk-heading-large uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-top" uk-scrollspy-class="" style="">        Sigal Sport    </h2>
-							<div class="uk-text-lead uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-bottom" uk-scrollspy-class="uk-animation-slide-bottom" style="">build – race – chill</div>
-						</div>
-					</div>
+
+
+<div class="uk-child-width-1-2@s uk-grid" uk-grid="">
+    <div class="uk-first-column">
+		<h1 class="uk-h5 uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-top" uk-scrollspy-class="" style="">        Tuning &amp; Performance    </h1>
+		<h2 class="uk-heading-large uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-top" uk-scrollspy-class="" style="">        Sigal Sport    </h2>
+		<div class="uk-text-lead uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-bottom" uk-scrollspy-class="uk-animation-slide-bottom" style="">build – race – chill</div>
+		<div class="uk-text-lead uk-margin-small uk-text-center uk-scrollspy-inview uk-animation-slide-bottom" uk-scrollspy-class="uk-animation-slide-bottom" style="">
+			<a href="https://www.youtube.com/channel/UCGK9pHoS6yS9GnfKg0lL8vw" uk-icon="youtube"></a>
+			<a href="https://www.instagram.com/sigalsport/" uk-icon="instagram"></a>
+			<a href="" uk-icon="facebook"></a>
+		</div>
+    </div>
+    <div>
+		<div class="uk-card uk-card-default uk-card-body uk-width-2-3@m uk-align-right">
+			<div class="uk-card-badge uk-label">HIGHLIGHT #22 12/2/17</div>
+			<p class="uk-card-title">POC Orange cup race</p>
+			<div uk-lightbox>
+				
+				<a class="" href="https://www.youtube.com/watch?v=n-jy-2F3Z6Q" data-caption="YouTube"><img alt="POC Orange cup race #22 12/2/17" src="highlight.JPG" style="height: 200px;"></a>
+
+			</div>
+		</div>
+    </div>
+</div>
+
+
+
 				</div>
             </div>
         </div>
